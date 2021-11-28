bcf9db98812c34f7028c99f3a883d2e6be775da1
Sebastian Ponce <poncegramming@gmail.com>
2021-11-28 04:40:59 +0000

    example files

---
+++ example.py
@@ -0,0 +1,11 @@
+'''
+Example file to show the pythonization of arrayfuncs and its functions
+from the "Array" class.
+'''
+
+import cppyy
+cppyy.include("arrayfuncs.cpp") # load arrayfuncs
+cppyy.load_library("arrayfuncs")
+from cppyy.gbl import Array # arrayfuncs is loaded succesfully
+example_array = Array(10) # create a vector with a length of 10
+print("Generate vector with zeros", example_array.zeros())

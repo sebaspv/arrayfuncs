2671e0cd4af62e2fc1308d2164286e10219f1c9e
Sebastian Ponce <poncegramming@gmail.com>
2021-11-28 07:23:58 +0000

    better example

--- example.py
+++ example.py
@@ -5,5 +5,5 @@ from the "Array" class.
 import cppyy
 cppyy.include('arrayfuncs.cpp')
 from cppyy.gbl import Array
-example_array = Array(10, 10)
-print(example_array.zeros()) # the matrix gets created succesfully!
+example_array = Array(2, 3)
+print("Zero matrix:", example_array.zeros()) # the zeros matrix gets created succesfully!
